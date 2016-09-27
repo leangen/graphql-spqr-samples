0dddcaee562972a8f859b230fd780deba895cd2f
loshmee <smilosh@gmail.com>
2016-09-27 22:37:29 +0200

    Extended Entity query (polite Customer greeting example)

--- src/main/java/io/leangen/spqr/samples/demo/dto/Customer.java
+++ src/main/java/io/leangen/spqr/samples/demo/dto/Customer.java
@@ -16,4 +16,23 @@ public class Customer extends Person {
     public void setPersonalTitle(PersonalTitle personalTitle) {
         this.personalTitle = personalTitle;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+
+        Customer customer = (Customer) o;
+
+        return personalTitle == customer.personalTitle;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = super.hashCode();
+        result = 31 * result + (personalTitle != null ? personalTitle.hashCode() : 0);
+        return result;
+    }
 }
