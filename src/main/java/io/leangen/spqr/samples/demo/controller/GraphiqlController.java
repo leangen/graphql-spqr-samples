4d7ae5a230aa0393a2fa73c828b3ddb05eedad86
Milos Stojiljkovic <milos.stojiljkovic@irdeto.com>
2016-10-05 14:04:48 +0200

    Graphiql controler urls

--- src/main/java/io/leangen/spqr/samples/demo/controller/GraphiqlController.java
+++ src/main/java/io/leangen/spqr/samples/demo/controller/GraphiqlController.java
@@ -10,14 +10,13 @@ import org.springframework.web.servlet.ModelAndView;
  * Created by milos on 5-10-16.
  */
 @RestController
-@RequestMapping("/graphiql")
 public class GraphiqlController {
-    @RequestMapping(value = "/domain", method = RequestMethod.GET, produces = MediaType.TEXT_HTML_VALUE)
+    @RequestMapping(value = "/graphiql/domain", method = RequestMethod.GET, produces = MediaType.TEXT_HTML_VALUE)
     public ModelAndView indexAnnotation() {
         return new ModelAndView("/index-domain");
     }
     
-    @RequestMapping(value = "/annotation", method = RequestMethod.GET, produces = MediaType.TEXT_HTML_VALUE)
+    @RequestMapping(value = "/graphiql", method = RequestMethod.GET, produces = MediaType.TEXT_HTML_VALUE)
     public ModelAndView indexDomain() {
         return new ModelAndView("/index-annotation");
     }
