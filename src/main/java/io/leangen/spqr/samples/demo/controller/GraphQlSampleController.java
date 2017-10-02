7f1dfd3a0daa0fd9895ea2d30a0645ee8f8bfb55
Bojan Tomic <bojan.tomic@servicenow.com>
2017-10-02 14:13:11 +0200

    Cleaner error messages (needed only until graphql-java is updated to v4)

--- src/main/java/io/leangen/spqr/samples/demo/controller/GraphQlSampleController.java
+++ src/main/java/io/leangen/spqr/samples/demo/controller/GraphQlSampleController.java
@@ -1,8 +1,26 @@
 package io.leangen.spqr.samples.demo.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import graphql.ErrorType;
+import graphql.ExceptionWhileDataFetching;
 import graphql.ExecutionResult;
+import graphql.ExecutionResultImpl;
 import graphql.GraphQL;
 import graphql.GraphQLError;
+import graphql.language.SourceLocation;
 import graphql.schema.GraphQLSchema;
 import io.leangen.graphql.GraphQLSchemaGenerator;
 import io.leangen.graphql.metadata.strategy.query.AnnotatedResolverBuilder;
@@ -13,17 +31,6 @@ import io.leangen.spqr.samples.demo.query.annotated.SocialNetworkQuery;
 import io.leangen.spqr.samples.demo.query.annotated.VendorQuery;
 import io.leangen.spqr.samples.demo.query.unannotated.DomainQuery;
 import io.leangen.spqr.samples.demo.query.unannotated.ProductQuery;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Map;
-import java.util.stream.Collectors;
 
 @RestController
 public class GraphQlSampleController {
@@ -59,20 +66,43 @@ public class GraphQlSampleController {
     @PostMapping(value = "/graphql", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     @ResponseBody
     public Object indexFromAnnotated(@RequestBody Map<String, Object> request) {
-        final ExecutionResult executionResult = graphQlFromAnnotated.execute(request.get("query").toString());
+        ExecutionResult executionResult = graphQlFromAnnotated.execute((String) request.get("query"), (String) request.get("operationName"), (Object) null);
 
         if (!executionResult.getErrors().isEmpty()) {
-            return extractErrorResponse(executionResult);
+            return sanitize(executionResult);
         }
 
         return executionResult;
     }
 
     //Just mocking error handling
-    private Object extractErrorResponse(ExecutionResult executionResult) {
-        return executionResult.getErrors()
-                .stream()
-                .map(GraphQLError::getMessage)
-                .collect(Collectors.toList());
+    private ExecutionResult sanitize(ExecutionResult executionResult) {
+        return new ExecutionResultImpl(executionResult.getErrors().stream()
+                .peek(err -> LOGGER.error(err.getMessage()))
+                .map(this::sanitize)
+                .collect(Collectors.toList()));
+    }
+    
+    private GraphQLError sanitize(GraphQLError error) {
+        if (error instanceof ExceptionWhileDataFetching) {
+            return new GraphQLError() {
+                @Override
+                public String getMessage() {
+                    Throwable cause = ((ExceptionWhileDataFetching) error).getException().getCause();
+                    return cause instanceof InvocationTargetException ? ((InvocationTargetException) cause).getTargetException().getMessage() : cause.getMessage();
+                }
+
+                @Override
+                public List<SourceLocation> getLocations() {
+                    return error.getLocations();
+                }
+
+                @Override
+                public ErrorType getErrorType() {
+                    return error.getErrorType();
+                }
+            };
+        }
+        return error;
     }
 }
