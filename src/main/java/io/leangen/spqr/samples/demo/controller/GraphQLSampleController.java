3db1b0c52c65e536b9bb163b4cfdaaf94adc8f9a
Bojan Tomic <veggen@gmail.com>
2018-03-04 18:52:57 +0100

    Replaced the hacky response sanitization and the deprecated code

--- src/main/java/io/leangen/spqr/samples/demo/controller/GraphQLSampleController.java
+++ src/main/java/io/leangen/spqr/samples/demo/controller/GraphQLSampleController.java
@@ -1,5 +1,6 @@
 package io.leangen.spqr.samples.demo.controller;
 
+import graphql.ExecutionInput;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,18 +10,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.lang.reflect.InvocationTargetException;
-import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
-import graphql.ErrorType;
-import graphql.ExceptionWhileDataFetching;
 import graphql.ExecutionResult;
-import graphql.ExecutionResultImpl;
 import graphql.GraphQL;
-import graphql.GraphQLError;
-import graphql.language.SourceLocation;
 import graphql.schema.GraphQLSchema;
 import io.leangen.graphql.GraphQLSchemaGenerator;
 import io.leangen.graphql.metadata.strategy.query.AnnotatedResolverBuilder;
@@ -32,21 +25,23 @@ import io.leangen.spqr.samples.demo.query.annotated.VendorQuery;
 import io.leangen.spqr.samples.demo.query.unannotated.DomainQuery;
 import io.leangen.spqr.samples.demo.query.unannotated.ProductQuery;
 
-@RestController
-public class GraphQlSampleController {
-    private static final Logger LOGGER = LoggerFactory.getLogger(GraphQlSampleController.class);
+import javax.servlet.http.HttpServletRequest;
 
-    private final GraphQL graphQlFromAnnotated;
+@RestController
+public class GraphQLSampleController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(GraphQLSampleController.class);
+
+    private final GraphQL graphQL;
 
     @Autowired
-    public GraphQlSampleController(PersonQuery personQuery,
+    public GraphQLSampleController(PersonQuery personQuery,
                                    SocialNetworkQuery socialNetworkQuery,
                                    DomainQuery domainQuery,
                                    ProductQuery productQuery,
                                    VendorQuery vendorQuery) {
 
         //Schema generated from query classes
-        GraphQLSchema schemaFromAnnotated = new GraphQLSchemaGenerator()
+        GraphQLSchema schema = new GraphQLSchemaGenerator()
                 .withResolverBuilders(
                         //Resolve by annotations
                         new AnnotatedResolverBuilder(),
@@ -59,50 +54,19 @@ public class GraphQlSampleController {
                 .withOperationsFromSingleton(productQuery)
                 .withValueMapperFactory(new JacksonValueMapperFactory())
                 .generate();
-        graphQlFromAnnotated = GraphQL.newGraphQL(schemaFromAnnotated).build();
+        graphQL = GraphQL.newGraphQL(schema).build();
+
         LOGGER.info("Generated GraphQL schema using SPQR");
     }
 
     @PostMapping(value = "/graphql", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     @ResponseBody
-    public Object indexFromAnnotated(@RequestBody Map<String, Object> request) {
-        ExecutionResult executionResult = graphQlFromAnnotated.execute((String) request.get("query"), (String) request.get("operationName"), (Object) null);
-
-        if (!executionResult.getErrors().isEmpty()) {
-            return sanitize(executionResult);
-        }
-
-        return executionResult;
-    }
-
-    //Just mocking error handling
-    private ExecutionResult sanitize(ExecutionResult executionResult) {
-        return new ExecutionResultImpl(executionResult.getErrors().stream()
-                .peek(err -> LOGGER.error(err.getMessage()))
-                .map(this::sanitize)
-                .collect(Collectors.toList()));
-    }
-    
-    private GraphQLError sanitize(GraphQLError error) {
-        if (error instanceof ExceptionWhileDataFetching) {
-            return new GraphQLError() {
-                @Override
-                public String getMessage() {
-                    Throwable cause = ((ExceptionWhileDataFetching) error).getException().getCause();
-                    return cause instanceof InvocationTargetException ? ((InvocationTargetException) cause).getTargetException().getMessage() : cause.getMessage();
-                }
-
-                @Override
-                public List<SourceLocation> getLocations() {
-                    return error.getLocations();
-                }
-
-                @Override
-                public ErrorType getErrorType() {
-                    return error.getErrorType();
-                }
-            };
-        }
-        return error;
+    public Map<String, Object> indexFromAnnotated(@RequestBody Map<String, String> request, HttpServletRequest raw) {
+        ExecutionResult executionResult = graphQL.execute(ExecutionInput.newExecutionInput()
+                .query(request.get("query"))
+                .operationName(request.get("operationName"))
+                .context(raw)
+                .build());
+        return executionResult.toSpecification();
     }
 }
